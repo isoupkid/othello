a92a84a3c72ed67f9826afeeeb1b839e0d97a172
isoupkid <blue@caltech.edu>
2016-03-07 17:54:11 -0800

    Added comments, some memory management

--- player.cpp
+++ player.cpp
@@ -24,6 +24,7 @@ Player::Player(Side side) {
  * Destructor for the player.
  */
 Player::~Player() {
+    delete board;
 }
 
 /*
@@ -39,25 +40,38 @@ Player::~Player() {
  * return NULL.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
+    // First do the opponent's move
     board->doMove(opponentsMove, opSide);
+
+    // Find all possible moves
     std::vector<Move*> possible = possibleMoves(mySide, board);
+    // If no possible moves, must pass.
     if (possible.size() == 0)
     {
         return NULL;
     }
+    // Find best move via minimax
     Move * best_move = miniMax(possible);
+    // Update internal board
     board->doMove(best_move, mySide);
     return best_move;
 }
 
+/*
+ * Finds and returns all the possible moves, given a side and a board
+ * to search.
+ */
 std::vector<Move*> Player::possibleMoves(Side side, Board * board)
 {
     std::vector<Move*> possible;
+    // Loop through all positions of the board
     for (int x = 0; x < 8; x++)
     {
         for (int y = 0; y < 8; y++)
         {
+            // Create a new move to check
             Move * move = new Move(x, y);
+            // Check if that move is legal; if so, add to vector
             if (board->checkMove(move, side))
             {
                 possible.push_back(move);
@@ -67,45 +81,70 @@ std::vector<Move*> Player::possibleMoves(Side side, Board * board)
     return possible;
 }
 
+/*
+ * Implementation of naive heuristics approach - determines the best
+ * move by maximizing current score, and returns that move.
+ */
 Move *Player::simpleHeuristics(std::vector<Move*> possible)
 {
+    // Initialize best_score to be the smallest int
     int best_score = std::numeric_limits<int>::min();
     Move * best_move;
+    // Loop through all possible moves
     for (unsigned int i = 0; i < possible.size(); i++)
     {
+        // Try each move on a new copy of the board
         Board * copy = board->copy();
         copy->doMove(possible[i], mySide);
+        // Calculate score of that move
         int score = copy->count(mySide) - copy->count(opSide);
         if (score > best_score)
         {
             best_score = score;
             best_move = possible[i];
         }
+        // Delete the dynamically allocated copy.
+        delete copy;
     }
     return best_move;
 }
 
+/*
+ * Returns the private field board
+ */
 Board *Player::getBoard()
 {
     return board;
 }
 
+/*
+ * Implementation of 2-ply minimax approach - aims to minimize the
+ * opponent's gain while maximizing own gain. Returns the move that
+ * does that.
+ */
 Move *Player::miniMax(std::vector<Move*> possible)
 {
 
     Move * best_move;
+    // Initialize best_score to be the lowest int possible
     int best_score = std::numeric_limits<int>::min();
 
-
+    // Loop through all possible moves
     for (unsigned int i = 0; i < possible.size(); i++)
     {
+        // Initialize worst_score, which keeps track of the worst-case
+        // scenario for a particular move.
         int worst_score = std::numeric_limits<int>::max();
 
         Board * copy = board->copy();
         copy->doMove(possible[i], mySide);
 
+        // Find all moves possible for the opponent as a result of this
+        // move.
         std::vector<Move*> opMoves = possibleMoves(opSide, copy);
 
+        // Loop through all of the opponent's moves & find the worst-
+        // case scenario.
         for (unsigned int j = 0; j < opMoves.size(); j++)
         {
             Board * copy2 = copy->copy();
@@ -116,12 +155,15 @@ Move *Player::miniMax(std::vector<Move*> possible)
             {
                 worst_score = score;
             }
+            delete copy2;
         }
+        // Find the least-worst case scenario. That is the best move.
         if (worst_score > best_score)
         {
             best_score = worst_score;
             best_move = possible[i];
         }
+        delete copy;
     }
     return best_move;
 }
